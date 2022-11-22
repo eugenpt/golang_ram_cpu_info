ab939e1e444e5eb964c4c4416ecf34683999f305
ep <eugen.pt@gmail.com>
2022-11-22 19:31:02 +0700

    fix..

--- golang_ram_cpu_info.go
+++ golang_ram_cpu_info.go
@@ -55,7 +55,6 @@ func printstuff() {
 }
 
 func onReady() {
-    systray.SetTemplateIcon(icon.Data, icon.Data)
     systray.SetTitle("Awesome App")
     systray.SetTooltip("Lantern")
     mQuitOrig := systray.AddMenuItem("Quit", "Quit the whole app")
