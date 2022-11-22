8e1c6262680e8c33c009108d44c00bef23074041
ep <eugen.pt@gmail.com>
2022-11-22 17:00:32 +0700

    +RAM usage

--- go.mod
+++ go.mod
@@ -8,13 +8,23 @@ require (
 )
 
 require (
+	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/getlantern/context v0.0.0-20190109183933-c447772a6520 // indirect
 	github.com/getlantern/errors v0.0.0-20190325191628-abdb3e3e36f7 // indirect
 	github.com/getlantern/golog v0.0.0-20190830074920-4ef2e798c2d7 // indirect
 	github.com/getlantern/hex v0.0.0-20190417191902-c6586a6fe0b7 // indirect
 	github.com/getlantern/hidden v0.0.0-20190325191715-f02dbb02be55 // indirect
 	github.com/getlantern/ops v0.0.0-20190325191751-d70cb0d6f85f // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/oxtoacart/bpool v0.0.0-20190530202638-03653db5a59c // indirect
-	golang.org/x/sys v0.0.0-20200515095857-1151b9dac4a9 // indirect
+	github.com/pbnjay/memory v0.0.0-20210728143218-7b4eea64cf58 // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
+	github.com/sbinet/go-eval v0.0.0-20160521182218-34e015998e32 // indirect
+	github.com/shirou/gopsutil/v3 v3.22.10 // indirect
+	github.com/yusufpapurcu/wmi v1.2.2 // indirect
+	golang.org/x/crypto v0.3.0 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/term v0.2.0 // indirect
 )
--- golang_ram_cpu_info.go
+++ golang_ram_cpu_info.go
@@ -2,11 +2,13 @@ package main
 
 import (
     "fmt"
-    "io/ioutil"
+    // "io/ioutil"
     "time"
 
     "github.com/getlantern/systray"
     "github.com/getlantern/systray/example/icon"
+
+    "github.com/shirou/gopsutil/v3/mem"
 )
 
 //     "github.com/skratchdot/open-golang/open"
@@ -16,10 +18,34 @@ func main() {
 
     onExit := func() {
         now := time.Now()
-        ioutil.WriteFile(fmt.Sprintf(`on_exit_%d.txt`, now.UnixNano()), []byte(now.String()), 0644)
+        // ioutil.WriteFile(fmt.Sprintf(`on_exit_%d.txt`, now.UnixNano()), []byte(now.String()), 0644)
+        fmt.Println("%v", now)
     }
 
     systray.Run(onReady, onExit)
+}
+
+func toGB(b uint64) float32 {
+    return float32(b) / (1024.0 * 1024 * 1024)
+}
+
+func mem_str_one(used uint64, total uint64) string {
+    return fmt.Sprintf("%.1f/%.1fGB %.0f%% %.1f GB Free", toGB(used), toGB(total), 100.0*float32(used)/float32(total), toGB(total-used))
+}
+
+func mem_str() string {
+    ram, _ := mem.SwapMemory()
+    phys_ram, _ := mem.VirtualMemory()
+
+    return fmt.Sprintf("RAM : %s\nPhys: %s", mem_str_one(ram.Used, ram.Total), mem_str_one(phys_ram.Used, phys_ram.Total))
+}
+
+func update() {
+    systray.SetTooltip(mem_str())
+}
+
+func printstuff() {
+    fmt.Println(mem_str())
 }
 
 func onReady() {
@@ -33,4 +59,16 @@ func onReady() {
         systray.Quit()
         fmt.Println("Finished quitting")
     }()
+
+    uptimeTicker := time.NewTicker(1 * time.Second)
+    dateTicker := time.NewTicker(3 * time.Second)
+
+    for {
+        select {
+        case <-uptimeTicker.C:
+            go update()
+        case <-dateTicker.C:
+            go printstuff()
+        }
+    }
 }
