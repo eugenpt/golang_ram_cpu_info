494be94bdd6afd83e0caaa0315d6849a2a54024d
ep <eugen.pt@gmail.com>
2022-11-22 15:31:39 +0700

    + show icon

--- golang_ram_cpu_info.go
+++ golang_ram_cpu_info.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "io/ioutil"
+    "time"
+
+    "github.com/getlantern/systray"
+    "github.com/getlantern/systray/example/icon"
+)
+
+//     "github.com/skratchdot/open-golang/open"
 
 func main() {
     fmt.Println("Hello, World!")
+
+    onExit := func() {
+        now := time.Now()
+        ioutil.WriteFile(fmt.Sprintf(`on_exit_%d.txt`, now.UnixNano()), []byte(now.String()), 0644)
+    }
+
+    systray.Run(onReady, onExit)
+}
+
+func onReady() {
+    systray.SetTemplateIcon(icon.Data, icon.Data)
+    systray.SetTitle("Awesome App")
+    systray.SetTooltip("Lantern")
+    mQuitOrig := systray.AddMenuItem("Quit", "Quit the whole app")
+    go func() {
+        <-mQuitOrig.ClickedCh
+        fmt.Println("Requesting quit")
+        systray.Quit()
+        fmt.Println("Finished quitting")
+    }()
 }
